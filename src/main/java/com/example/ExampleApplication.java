cbe427b9fc6ffd5b763b387c1095aa209480715d
sweytha <sweythak@gmail.com>
2022-11-13 18:40:33 -0500

    example first commit

---
+++ src/main/java/com/example/ExampleApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class ExampleApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);	
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+    Logger logger=LoggerFactory.getLogger(this.getClass());
+    @GetMapping("/hello")
+	public String hello(){
+    	
+    	String response="Hello world";
+    	logger.info("Response from the application logs {}",response);
+		
+		return "browser output";
+	
+		
+	}
+	
+	
+	
+
+}
